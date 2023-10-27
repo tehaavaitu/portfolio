5b41b241c25440d27a1af349699e9c5493d15ba6
tehaavaitu <tehaa972@gmail.com>
2023-10-27 09:03:02 -0400

    first

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute('style', 'top:'+(e.pageY - 20)+"px; left:"+(e.pageX - 20)+"px;")
+})
+
+document.addEventListener('click', ()=>{
+    cursor.classList.add('expand');
+
+    setTimeout(()=>{
+        cursor.classList.remove("expand");
+    }, 500);
+})
+
+$(document).ready(function(){			  
+	$('li a').click(function(){
+	
+		$('ul .navbar-nav').slideToggle()
+	});		  			  
+})
